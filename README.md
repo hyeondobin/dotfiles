abb6312b1793730850ff2cb358eef92115879201
hyeondobin <dobinhyeon@gmail.com>
2023-12-18 18:59:50 +0900

    edit README | remove dirs edit README remove directories used before

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Hyeondobin's dotfiles
+
+## Install chezmoi
+``
+pacman -S chezmoi
+``
+* Init chezmoi
+``
+chezmoi init https://github.com/hyeondobin/dotfiles.git
+``
+
+## I
--- fish/functions/eh.fish
+++
@@ -1,4 +0,0 @@
-function eh --wraps='nvim ~/.config/hypr/hyprland.conf' --description 'alias eh nvim ~/.config/hypr/hyprland.conf'
-  nvim ~/.config/hypr/hyprland.conf $argv
-        
-end
