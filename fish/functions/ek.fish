f81c2093cb342dcc8bcdb390e29b726879580279
hyeondobin <dobinhyeon@gmail.com>
2024-01-01 16:52:52 +0900

    add alias ek to edit kitty config

---
+++ fish/functions/ek.fish
@@ -0,0 +1,4 @@
+function ek --wraps='vi ~/.config/kitty/' --wraps='vi ~/.config/kitty/kitty.conf' --description 'alias ek vi ~/.config/kitty/kitty.conf'
+  vi ~/.config/kitty/kitty.conf $argv
+        
+end
