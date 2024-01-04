77d277e31e4d3cf5bf98e3a3c44a0a6c3b85a339
hyeondobin <dobinhyeon@gmail.com>
2024-01-04 15:00:13 +0000

    update install.sh
    now check font and programs exist and install it

--- install.sh
+++ install.sh
@@ -11,24 +11,24 @@ ENDCOLOR="\e[0m"
 printf "# Syncing to home folder ... \n"
 
 function syncFile() {
-	local sourceFile="$1"
-	if [ ! -d "$HOME/.config/${sourceFile}" ]; then
-		printf "${BLUE}Symlink:${ENDCOLOR} ${sourceFile}\n"
-		ln -sf "$CWD/${sourceFile}" "$HOME/.config/${sourceFile}"
-		printf "Done\n"
-	else
-		printf "${YELLOW}Skipping${ENDCOLOR} ${sourceFile}\n"
-	fi
+    local sourceFile="$1"
+    if [ ! -d "$HOME/.config/${sourceFile}" ]; then
+        printf "${BLUE}Symlink:${ENDCOLOR} ${sourceFile}\n"
+        ln -sf "$CWD/${sourceFile}" "$HOME/.config/${sourceFile}"
+        printf "Done\n"
+    else
+        printf "${YELLOW}Skipping${ENDCOLOR} ${sourceFile}\n"
+    fi
 }
 function syncFileHome() {
-	local sourceFile="$1"
-	if [ ! -d "$HOME/${sourceFile}" ]; then
-		printf "${BLUE}Symlink:${ENDCOLOR} ${sourceFile}\n"
-		ln -sf "$CWD/${sourceFile}" "$HOME/${sourceFile}"
-		printf "Done\n"
-	else
-		printf "${YELLOW}Skipping${ENDCOLOR} ${sourceFile}\n"
-	fi
+    local sourceFile="$1"
+    if [ ! -d "$HOME/${sourceFile}" ]; then
+        printf "${BLUE}Symlink:${ENDCOLOR} ${sourceFile}\n"
+        ln -sf "$CWD/${sourceFile}" "$HOME/${sourceFile}"
+        printf "Done\n"
+    else
+        printf "${YELLOW}Skipping${ENDCOLOR} ${sourceFile}\n"
+    fi
 }
 
 syncFile "fish"
@@ -41,5 +41,42 @@ syncFile "fcitx5"
 syncFile "nvim"
 
 syncFileHome "Scripts"
+syncFileHome ".gitconfig"
+
+if command -v yay &> /dev/null
+then
+    echo "yay already installed"
+else
+    printf "Installing 'YAY'"
+    sudo pacman -S --needed git base-devel
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si
+fi
+
+if command -v fish &> /dev/null
+then
+    echo "fish already installed"
+else
+    printf "Installing 'fish'"
+    yay fish
+fi
+  
+is_font_installed(){
+    fontname=$1
+    fc-list | grep -i "$fontname" >/dev/null
+}
+install_font(){
+    fontname=$1
+if ! is_font_installed $1; then 
+    printf "Installing $fontname"
+    yay "$fontname"
+else
+    printf "$fontname already installed\n"
+fi
+}
+
+install_font "FiraCode Nerd Font"
+install_font "D2Coding"
 
 printf "${GREEN}Done${ENDCOLOR}\n"
