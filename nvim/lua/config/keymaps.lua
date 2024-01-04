5bf8825cea1cec89d7d4c45de8708dc78b22ef91
hyeondobin <dobinhyeon@gmail.com>
2024-01-04 10:41:46 +0900

    update nvim config
    add live-server.nvim and keymap to call it
    change neo-tree to close window when file opend
    change stylua.toml to use indent_width = 4

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,6 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 local opts = { silent = true, remap = false }
+local wk = require("which-key")
 
 -- add ; to end of line
 vim.keymap.set("i", ";<Space>", "; ", opts)
@@ -10,3 +11,12 @@ vim.keymap.set("i", ";", "<C-c>A;", opts)
 -- move line up/down with Alt
 vim.keymap.set("n", "<A-j>", "ddp", opts)
 vim.keymap.set("n", "<A-k>", "ddkP", opts)
+
+-- https://github.com/folke/which-key.nvim?tab=readme-ov-file#%EF%B8%8F-mappings
+wk.register({
+    p = {
+        name = "preview",
+        s = { "<cmd>LiveServerStart<CR>", "Start LiveServer" },
+        c = { "<cmd>LiveServerStop<CR>", "Stop LiveServer" },
+    },
+}, { prefix = "<leader>" })
--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,10 +1,18 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  opts = {
-    filesystem = {
-      filtered_items = {
-        visible = true,
-      },
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+        filesystem = {
+            filtered_items = {
+                visible = true,
+            },
+        },
+        event_handlers = {
+            {
+                event = "file_opened",
+                handler = function(file_path)
+                    require("neo-tree.command").execute({ action = "close" })
+                end,
+            },
+        },
     },
-  },
 }
