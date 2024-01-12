a01a3f04a33635d8681166d3bf11c174751bbd89
hyeondobin <dobinhyeon@gmail.com>
2024-01-12 18:41:29 +0900

    fix alias to use eza

--- fish/functions/l.fish
+++ fish/functions/l.fish
@@ -1,4 +1,4 @@
-function l --wraps='ls -lah' --description 'alias l ls -lah'
-  ls -lah $argv
-        
+function l --wraps='eza -lah' --description 'alias l eza -lah'
+    eza -lah --git --git-repos $argv
+
 end
