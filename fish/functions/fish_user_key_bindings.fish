f3a10e59b300d68cf07d5e6c6aa1f8c4239ff263
hyeondobin <dobinhyeon@gmail.com>
2024-01-15 16:28:10 +0900

    disable alt/super v to open editor

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -3,4 +3,8 @@ function fish_user_key_bindings
     bind -M insert -m default \cc force-repaint
     bind -M insert \el forward-word
     bind -M insert \cu accept-autosuggestion
+    bind --preset \ev true
+    bind -M insert \ev true
+    bind -M visual \ev true
+    bind -M normal \ev true
 end
