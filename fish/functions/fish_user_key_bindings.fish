bcfad9421717cb0a2de19820fb27ef56da8ffdd2
hyeondobin <dobinhyeon@gmail.com>
2024-01-15 16:05:46 +0900

    add bind to accept suggestion

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,4 +1,6 @@
 function fish_user_key_bindings
-	fish_vi_key_bindings
-	bind -M insert -m default \cc backward-char force-repaint
+    fish_vi_key_bindings
+    bind -M insert -m default \cc force-repaint
+    bind -M insert \el forward-word
+    bind -M insert \cu accept-autosuggestion
 end
