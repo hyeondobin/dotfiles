ec509c6acaa8ada88d9fca289210c4b9cc69aa20
hyeondobin <dobinhyeon@gmail.com>
2024-01-17 17:04:23 +0900

    upgrade clone.sh

--- clone.sh
+++ clone.sh
@@ -1,13 +1,47 @@
 #!/usr/bin/env bash
 
-clone ()
-{
-    git clone https://github.com/$1.git ~/Clones/$2
+clone() {
+	git clone https://github.com/$1.git ~/Clones/$2
 }
+jobsLeft=true
+while [[ $jobsLeft = true ]]; do
 
-if  command -v git &>/dev/null 
-    then
-        clone skywind3000/z.lua z.lua
-fi
+	printf "Choose which to clone\n"
+	printf "\n"
+	printf "1. z.lua\n"
+	printf "2. rofi theme collection\n"
+	printf "3. tmux plugin manager(tpm)\n"
 
+	read -p "Choose (eg: 1 or [a]ll) >> " answer
+	if command -v git &>/dev/null; then
+		case $answer in
+		1)
+			clone skywind3000/z.lua z.lua
+			jobsLeft=false
+			;;
+		2)
+			if [[ ! -e ~/Clones/rofi-themes-collection ]]; then
+				clone newmanls/rofi-themes-collection rofi-themes-collection
+			fi
+			mkdir -p ~/.local/share/rofi/themes
+			cp ~/Clones/rofi-themes-collection/themes/rounded-common.rasi ~/.local/share/rofi/themes/
+			cp ~/Clones/rofi-themes-collection/themes/rounded-nord-dark.rasi ~/.local/share/rofi/themes/
+			jobsLeft=false
+			;;
+		3)
+			clone tmux-plugins/tpm tpm
+			jobsLeft=false
+			;;
+		a)
+			clone skywind3000/z.lua z.lua
+			clone newmanls/rofi-themes-collection rofi-themes-collection
+			jobsLeft=false
+			;;
+		*)
+			printf "Choose right answer\n"
+			printf "\n"
+			;;
+		esac
+	fi
 
+done
