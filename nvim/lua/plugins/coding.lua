ab43c3bade47f0876ff3b6929930549a2e95bb48
hyeondobin <dobinhyeon@gmail.com>
2024-01-19 10:38:20 +0900

    add harpoon

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -1,4 +1,35 @@
 return {
-    "echasnovski/mini.pairs",
-    enabled = false,
+    {
+        "echasnovski/mini.pairs",
+        enabled = false,
+    },
+    {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            local harpoon = require("harpoon")
+            harpoon:setup()
+
+            vim.keymap.set("n", "<leader>a", function()
+                harpoon:list():append()
+            end)
+            vim.keymap.set("n", "<C-e>", function()
+                harpoon.ui:toggle_quick_menu(harpoon:list())
+            end)
+
+            vim.keymap.set("n", "<M-h>", function()
+                harpoon:list():select(1)
+            end)
+            vim.keymap.set("n", "<M-t>", function()
+                harpoon:list():select(2)
+            end)
+            vim.keymap.set("n", "<M-n>", function()
+                harpoon:list():select(3)
+            end)
+            vim.keymap.set("n", "<M-s>", function()
+                harpoon:list():select(4)
+            end)
+        end,
+    },
 }
--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -55,7 +55,7 @@ return {
         event = "VeryLazy",
         opts = {
             options = {
-                mode = "tabs",
+                mode = "buffers",
                 show_buffer_close_icons = false,
                 show_close_icon = false,
             },
