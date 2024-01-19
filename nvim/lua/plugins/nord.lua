648dda847a5cbe43cc74cb67cf6f8e0427c4d5cd
hyeondobin <dobinhyeon@gmail.com>
2024-01-19 22:29:44 +0900

    enable background none

--- nvim/lua/plugins/nord.lua
+++ nvim/lua/plugins/nord.lua
@@ -3,9 +3,9 @@ function ColorMyPencils(color)
     color = color or "nord"
     vim.cmd.colorscheme(color)
 
-    -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    -- vim.api.nvim_set_hl(0, "NotifyBackground", { bg = "#434c5e" })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NotifyBackground", { bg = "#434c5e" })
 end
 
 return {
