9df8297e2176b9de9000217dce180f405c13f5fb
hyeondobin <dobinhyeon@gmail.com>
2024-01-20 00:07:29 +0900

    move code to editor.lua

---
+++ nvim/lua/plugins/editor.lua
@@ -0,0 +1,24 @@
+return {
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        opts = {
+            filesystem = {
+                filtered_items = {
+                    visible = true,
+                },
+            },
+            event_handlers = {
+                {
+                    event = "file_opened",
+                    handler = function(file_path)
+                        require("neo-tree.command").execute({ action = "close" })
+                    end,
+                },
+            },
+        },
+    },
+    {
+        "folke/flash.nvim",
+        enabled = false,
+    },
+}
