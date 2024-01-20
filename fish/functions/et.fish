2681d72db6c29c98e862659266ff3bfb30e335db
hyeondobin <dobinhyeon@gmail.com>
2024-01-20 12:42:48 +0900

    add alias for edit tmux

---
+++ fish/functions/et.fish
@@ -0,0 +1,4 @@
+function et --wraps='nvim /home/hyeondobin/.config/tmux/tmux.conf' --description 'alias et nvim /home/hyeondobin/.config/tmux/tmux.conf'
+    nvim /home/hyeondobin/.config/tmux/tmux.conf
+
+end
