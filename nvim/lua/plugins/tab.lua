93e29c53490bf3060fe2b3a80948620a87556874
hyeondobin <dobinhyeon@gmail.com>
2024-01-23 18:13:43 +0900

    fix: tab now accept

--- nvim/lua/plugins/tab.lua
+++ nvim/lua/plugins/tab.lua
@@ -29,19 +29,20 @@ return {
                 ["<Tab>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         -- cmp.select_next_item()
+                        cmp.confirm()
                         -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
                         -- this way you will only jump inside the snippet region
-                        fallback()
                     elseif luasnip.expand_or_locally_jumpable() then
-                        luasnip.expand_or_locally_jump()
+                        luasnip.expand_or_jump()
+                    elseif has_words_before() then
+                        cmp.confirm()
                     else
                         fallback()
                     end
                 end, { "i", "s" }),
                 ["<S-Tab>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
-                        -- cmp.select_prev_item()
-                        fallback()
+                        cmp.select_prev_item()
                     elseif luasnip.jumpable(-1) then
                         luasnip.jump(-1)
                     else
