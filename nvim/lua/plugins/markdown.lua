fc9c228b2f7ab28fc3a97fb1a2dd50d7b61a8053
hyeondobin <dobinhyeon@gmail.com>
2024-01-31 00:08:29 +0900

    install obsidian nvim

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -10,4 +10,26 @@ return {
         mkdp_auto_start = 1,
         mkdp_browser = "F:/Users/Dobin/AppData/Local/Vivaldi/Application/vivaldi.exe",
     },
+    if vim.loop.os_uname().sysname == "Windows_NT" then
+    {
+        "epwalsh/obsidian.nvim",
+        version = "*",
+        lazy = true,
+        event = {
+                "BufReadPre /mnt/f/Users/Dobin/Obsidian/HyeonDobin/**.md",
+                "BufNewFile /mnt/f/Users/Dobin/Obsidian/HyeonDobin/**.md",
+        },
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+        },
+        opts = {
+            workspaces = {
+                {
+                    name = "HyeonDobin",
+                    path = "/mnt/f/Users/Dobin/Obsidian/HyeonDobin",
+                },
+            }
+        }
+    },
+end
 }
