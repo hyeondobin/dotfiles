947f0c208e5209f5012a59c56729999feee2671f
hyeondobin <dobinhyeon@gmail.com>
2024-02-11 15:44:05 +0900

    delete nvim from repo

--- nvim/lua/plugins/live-server.lua
+++
@@ -1,8 +0,0 @@
-return {
-    {
-        "barrett-ruth/live-server.nvim",
-        build = "npm install -g live-server",
-        event = { "BufReadPre", "BufNewFile" },
-        config = true,
-    },
-}
