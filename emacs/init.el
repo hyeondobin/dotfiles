71e35b2fc0c5a5e1d2bd0518f6888c9873d4826d
Hyeon Dobin <77524773+hyeondobin@users.noreply.github.com>
2025-02-28 22:20:26 +0900

    edit emacs config

--- emacs/init.el
+++ emacs/init.el
@@ -1,4 +1,5 @@
-; elpaca installer
+;;  elpaca installer
+;;; Code:
 (defvar elpaca-installer-version 0.10)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
@@ -37,14 +38,20 @@
     (load "./elpaca-autoloads")))
 (add-hook 'after-init-hook #'elpaca-process-queues)
 (elpaca `(,@elpaca-order))
-					; end of elpaca installer
-					(setq use-package-always-ensure t)
 
 ; enable elpaca use package 
 (elpaca elpaca-use-package
   (elpaca-use-package-mode))
 
+
+;; end of elpaca installer
+
+(setq use-package-always-ensure t)
+
+(use-package diminish)
+
 (use-package general ; key bindings manager? kinda
+  :after evil
   :config
   (general-evil-setup t)
   (general-create-definer dobin/leader-keys
@@ -52,29 +59,34 @@
 			  :prefix ","
 			  :global-prefix "C-,")
   (dobin/leader-keys
-    "w" '(save-buffer :which-key "Save buffer")))
+    "w" '(save-buffer :which-key "Save buffer")
+    "f" '(:ignore t :which-key "Files")
+    "fr" '(recentf :which-key "Files - recent")
+    )
+  )
 
 ;; evil
 (defun dobin/evil-hook ()
-  (dolist (mode '(custom-mode
-		  eshell-mode
-		  git-rebase-mode
-		  erc-mode
-		  circe-server-mode
-		  circe-chat-mode
-		  circe-query-mode
-		  sauron-mode
-		  term-mode))
-    (add-to-list 'evil-emacs-state-modes mode)))
+  (dolist (modesList '(custom-mode
+		       eshell-mode
+		       git-rebase-mode
+		       erc-mode
+		       circe-server-mode
+		       circe-chat-mode
+		       circe-query-mode
+		       sauron-mode
+		       term-mode))
+    (add-to-list 'evil-emacs-state-modes modesList)))
 (use-package evil
   :init
   (setq evil-want-integration t)
   (setq evil-want-keybinding nil)
   (setq evil-want-C-u-scroll t)
-  (setq evil-want-C-i-jump nil)
-  :hook (evil-mode . dobin/evil-hook)
+  (setq evil-want-C-i-jump t)
+  (setq evil-vsplit-window-right t)
+  (setq evil-split-window-below t)
+  (evil-mode)
   :config
-  (evil-mode 1)
   (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
   (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
 
@@ -88,20 +100,23 @@
   :after evil
   :config
   (evil-collection-init))
+(use-package evil-numbers
+  :after evil)
 
-(use-package hydra)
-(defhydra hydra-text-scale (:timeout 4)
+(use-package hydra
+  :after general)
+(add-hook 'elpaca-after-init-hook (lambda () (defhydra hydra-text-scale (:timeout 4)
 	  ("j" text-scale-increase "in")
 	  ("k" text-scale-decrease "out")
-	  ("f" nil "finished" :exit t))
-(defhydra hydra-window-scale (:timeout 4)
+	  ("f" nil "finished" :exit t))))
+(add-hook 'elpaca-after-init-hook (lambda () (defhydra hydra-window-scale (:timeout 4)
   ("j" evil-window-increase-width "less")
   ("k" evil-window-decrease-width "more")
   ("=" balance-windows "equal")
-  ("f" nil "finished" :exit t))
-(dobin/leader-keys
+  ("f" nil "finished" :exit t))))
+(add-hook 'elpaca-after-init-hook (lambda () (dobin/leader-keys
   "st" '(hydra-text-scale/body :which-key "scale text")
-  "sw" '(hydra-window-scale/body :which-key "scale window"))
+  "sw" '(hydra-window-scale/body :which-key "scale window"))))
 
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
@@ -139,7 +154,26 @@
   (setq which-key-idle-delay 0.2))
 
 ; set font
-(set-face-attribute 'default nil :font "JetBrainsMono NF" :height 120)
+(set-face-attribute 'default nil
+		    :font "JetBrainsMono NF"
+		    :height 120
+		    :weight 'medium)
+(set-face-attribute 'variable-pitch nil
+		    :font "JetBrainsMono NF"
+		    :height 130
+		    :weight 'medium)
+(set-face-attribute 'fixed-pitch nil
+		    :font "JetBrainsMono NF"
+		    :height 120
+		    :weight 'medium)
+;; ref: https://gitlab.com/dwt1/configuring-emacs/-/blob/main/07-the-final-touches/config.org?ref_type=heads#evil
+(set-face-attribute 'font-lock-comment-face nil
+		    :slant 'italic)
+(set-face-attribute 'font-lock-keyword-face nil
+		    :slant 'italic)
+(add-to-list 'default-frame-alist '(font . "JetBrainsMono NF-12"))
+(setq-default line-spacing 0.13)
+
 
 ; theme
 (use-package catppuccin-theme
@@ -147,8 +181,50 @@
   (catppuccin-flavor 'macchiato))
 (add-hook 'elpaca-after-init-hook (lambda () (load-theme 'catppuccin :no-confirm)))
 
+;; completion - CO(mpletion in) R(egion) FU(unction)
+(use-package corfu
+  :custom
+  (corfu-cycle t)
+  (corfu-preview-current t)
+  (corfu-auto t)
+  (corfu-quit-no-match 'separator)
+  :init
+  (global-corfu-mode)
+  :config
+  (define-key corfu-map (kbd "<RET>") nil)
+  ;; ref: https://github.com/minad/corfu/issues/12#issuecomment-1171033435
+  (define-key corfu-map "\C-l" #'corfu-insert)
+  (define-key corfu-map "\C-y" #'corfu-insert)
+  (define-key corfu-map "\C-e" #'corfu-quit)
+  (define-key corfu-map "\C-n" #'corfu-next)
+  (define-key corfu-map "\C-p" #'corfu-previous)
+  )
+(use-package orderless
+  :custom
+  (completion-styles '(orderless basic))
+  (completion-category-defaults nil)
+  (completion-category-overrides '((file (styles partial-completion)))))
 
-; completion - ivy
+(use-package dashboard
+  :init
+  (setq initial-buffer-choice 'dashboard-open)
+  (setq dahshboard-set-heading-icons t)
+  (setq dashboard-set-file-icons t)
+  (setq dashboard-banner-logo-title "EMACS")
+  (setq dashboard-center-content nil)
+  (setq dashboard-items '((recents . 5)
+			  (agenda . 5)
+			  (bookmarks . 3)
+			  (projects . 3)
+			  (registers . 3)))
+  :custom
+  (dashboard-modify-heading-icons '((recents . "file-text")
+				    (bookmarks . "book")))
+  :config
+  (dashboard-setup-startup-hook))
+
+
+;; completion - ivy
 (use-package ivy
   :diminish ; diminish hides ivy from modes list? on bottom 
   :bind (("C-s" . swiper)
@@ -197,19 +273,19 @@
   (counsel-describe-function-function #'helpful-callable)
   (counsel-describe-variable-function #'helpful-variable)
   :bind
-  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-function] . counsel-describe-function) ;
   ([remap describe-command] . helpful-command)
   ([remap describe-variable] . counsel-describe-variable)
   ([remap describe-key] . helpful-key))
 ;; TODO: add describe-symbol
 
-(use-package all-the-icons)
+;; (use-package all-the-icons)
 
 ; modeline, for now following vid, TODO: find modeline or configure mine
-(use-package doom-modeline
-  :init (doom-modeline-mode 1)
-  :custom
-  (doom-modeline-height 15))
+;; (use-package doom-modeline
+;;   :init (doom-modeline-mode 1)
+;;   :custom
+;;   (doom-modeline-height 15))
 
 ;; git
 ;; (use-package magit)
@@ -221,3 +297,37 @@
 
 ;; or use :bind section of 'use-package'  
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages nil)
+ '(warning-suppress-log-types '((elpaca core 31.0.50))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; LSP-MODE
+(use-package lsp-mode
+  :hook (lsp-mode . lsp-enable-which-key-integration)
+  :commands lsp)
+(use-package lsp-nix
+  :after (lsp-mode)
+  :demand t
+  :custom
+  (lsp-nix-nil-formatter ["nixfmt"]))
+
+(use-package nix-mode
+  :hook (nix-mode . lsp-deferred))
+  ;; :mode "\\.nix\\'")
+
+(use-package lsp-ui
+  :commands lsp-ui-mode)
+
+
+;; (use-package nix-ts-mode
+;;   :mode "\\.nix\\'")
