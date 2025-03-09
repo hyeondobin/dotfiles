8111b8e59a0548f2c62312a55eab08adfde0b128
Hyeon Dobin <77524773+hyeondobin@users.noreply.github.com>
2025-03-09 14:15:27 +0900

    change font size | disable tab bar

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,5 +4,8 @@ local config = wezterm.config_builder()
 config.color_scheme = "Catppuccin Macchiato"
 
 config.font = wezterm.font("JetBrainsMono NF")
+config.font_size = 14
+
+config.enable_tab_bar = false
 
 return config
