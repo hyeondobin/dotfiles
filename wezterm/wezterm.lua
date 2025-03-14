cfda41470a26faacee58f6eed0cd8265dbdfc2a9
Hyeon Dobin <77524773+hyeondobin@users.noreply.github.com>
2025-03-14 23:22:26 +0900

    change font weight to "DemiBold"

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local config = wezterm.config_builder()
 
 config.color_scheme = "Catppuccin Macchiato"
 
-config.font = wezterm.font("JetBrainsMono NF")
+config.font = wezterm.font("JetBrainsMono NF", { weight = "DemiBold" })
 config.font_size = 14
 
 config.enable_tab_bar = false
