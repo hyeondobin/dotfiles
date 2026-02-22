758dcdd140fd1cad84df8f24fbe555d2688dbcc5
hyeondobin <dobinhyeon@gmail.com>
2026-02-22 20:10:03 +0900

    update emacs config

--- emacs/customs.el
+++ emacs/customs.el
@@ -1,14 +1,11 @@
 ;;; -*- lexical-binding: t -*-
-;; 커스터마이즈 모드에서 설정한 값이 lisp으로 어떻게 나오는 지 확인하기 위해 사용하는 파일.
-;; 실제 저장한 값이 이 파일로 저장되고 따로 init.el에서 로드하지 않기 때문에 저장한 값은
-;; 별도로 옮겨주지 않는 이상 다음 emacs 실행 시 유지되지 않는다.
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(recentf-mode t))
+ '(dired-auto-revert-buffer 'dired-directory-changed-p)
+ '(dired-kill-when-opening-new-dired-buffer t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
