0ec959404169d9c00f125cc142c92169e61b0651
Dobin Hyeon <dobinhyeon@gmail.com>
2026-02-22 19:33:59 +0900

    update wezterm config

--- Configs/wezterm/.config/wezterm/wezterm.lua
+++ Configs/wezterm/.config/wezterm/wezterm.lua
@@ -1,15 +1,25 @@
 local wezterm = require("wezterm")
+local cur_os = wezterm.target_triple
 local config = wezterm.config_builder()
+
+if cur_os:find("windows") then
+	config.default_prog = { "pwsh.exe", "-NoLogo" }
+end
 
 config.color_scheme = "Catppuccin Macchiato"
 
 config.font = wezterm.font("JetBrainsMono NF", { weight = "DemiBold" })
+config.font = wezterm.font_with_fallback({
+	"D2CodingLigature Nerd Font",
+})
 config.font_size = 14
 
-config.enable_tab_bar = false
+config.enable_tab_bar = true
 
 config.window_background_opacity = 0.75
 
 config.enable_wayland = false
+
+config.mux_enable_ssh_agent = false
 
 return config
